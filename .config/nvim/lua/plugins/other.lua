92a6bab41e05a3607e9d1cf80c0942f4f45a869b
zcysxy <zcysxy.wyzdz@gmail.com>
2024-02-06 23:21:14 +0800

    nvim plugin ob-bridge

--- .config/nvim/lua/plugins/other.lua
+++ .config/nvim/lua/plugins/other.lua
@@ -54,4 +54,17 @@ return {
 			require('oil').setup()
 		end
 	},
+
+	{
+		"oflisback/obsidian-bridge.nvim",
+		dependencies = { "nvim-telescope/telescope.nvim" },
+		config = function() require("obsidian-bridge").setup({
+			scroll_sync = true
+		}) end,
+		event = {
+			"BufReadPre *.md",
+			"BufNewFile *.md",
+		},
+		lazy = true,
+	}
 }
