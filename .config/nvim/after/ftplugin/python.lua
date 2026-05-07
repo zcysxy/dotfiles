1e8ea77481fce1cc37f39a7580b9ec9619173786
zcysxy <zcysxy.wyzdz@gmail.com>
2026-05-07 14:11:28 -0400

    python

--- .config/nvim/after/ftplugin/python.lua
+++ .config/nvim/after/ftplugin/python.lua
@@ -2,23 +2,23 @@
 
 -- BlockFolds equivalent in Lua for Neovim
 local function block_folds(lnum)
-  local thisline = vim.fn.getline(lnum)
-  if string.find(thisline, '# %%') then
-    return '>1'
-  else
-    return '='
-  end
+	local thisline = vim.fn.getline(lnum)
+	if string.find(thisline, '# %%') then
+		return '>1'
+	else
+		return '='
+	end
 end
 
 vim.opt.foldmethod = 'expr'
 vim.opt.foldexpr = 'v:lua.BlockFolds()'
 vim.opt.foldenable = false -- Disable folding at startup
 
-vim.keymap.set({ 'n' }, '<leader>ll', function ()
+vim.keymap.set({ 'n' }, '<leader>ll', function()
 	-- Check if the current buffer is has an extension of .ipynb
 	local buf_name = vim.api.nvim_buf_get_name(0)
 	if buf_name:match('%.ipynb$') then
-	vim.cmd('JupyterRunFile')
+		vim.cmd('JupyterRunFile')
 	else
 		vim.cmd('SlimeSend0')
 	end
@@ -26,3 +26,18 @@ end)
 
 -- Register the function globally for foldexpr
 _G.BlockFolds = block_folds
+
+vim.treesitter.query.set("python", "textobjects", [[
+	((comment) @cell.inner
+	 (#lua-match? @cell.inner "^# %%%%"))
+]])
+local move = require("nvim-treesitter-textobjects.move")
+local opts = { buffer = true, silent = true }
+
+vim.keymap.set({ "n", "x", "o" }, "]]", function()
+	move.goto_next_start("@cell.inner", "textobjects")
+end, vim.tbl_extend("force", opts, { desc = "Next Jupyter Cell" }))
+
+vim.keymap.set({ "n", "x", "o" }, "[[", function()
+	move.goto_previous_start("@cell.inner", "textobjects")
+end, vim.tbl_extend("force", opts, { desc = "Previous Jupyter Cell" }))
--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,63 +1,85 @@
 return {
-	'nvim-treesitter/nvim-treesitter',
-	config = function()
-		require 'nvim-treesitter.config'.setup {
-			-- A list of parser names, or "all" (the five listed parsers should always be installed)
-			ensure_installed = {
-				"c",
-				"lua",
-				"vim",
-				"vimdoc",
-				"query",
-				"python",
-				"markdown"
-			},
-			-- Install parsers synchronously (only applied to `ensure_installed`)
-			sync_install = false,
+	{
+		'nvim-treesitter/nvim-treesitter',
+		config = function()
+			require 'nvim-treesitter.config'.setup {
+				-- A list of parser names, or "all" (the five listed parsers should always be installed)
+				ensure_installed = {
+					"c",
+					"lua",
+					"vim",
+					"vimdoc",
+					"query",
+					"python",
+					"markdown"
+				},
+				-- Install parsers synchronously (only applied to `ensure_installed`)
+				sync_install = false,
 
-			-- Automatically install missing parsers when entering buffer
-			-- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-			auto_install = true,
+				-- Automatically install missing parsers when entering buffer
+				-- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+				auto_install = true,
 
-			-- List of parsers to ignore installing (for "all")
-			--ignore_install = { "javascript" },
+				-- List of parsers to ignore installing (for "all")
+				--ignore_install = { "javascript" },
 
-			---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
-			-- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
+				---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
+				-- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
 
-			highlight = {
-				enable = true,
+				highlight = {
+					enable = true,
 
-				-- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
-				-- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
-				-- the name of the parser)
-				-- list of language that will be disabled
-				disable = { "ipynb", "latex" },
-				additional_vim_regex_highlighting = { "latex", "markdown" },
-				-- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
-				--disable = function(lang, buf)
-				--local max_filesize = 100 * 1024 -- 100 KB
-				--local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-				--if ok and stats and stats.size > max_filesize then
-				--return true
-				--end
-				--end,
+					-- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
+					-- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
+					-- the name of the parser)
+					-- list of language that will be disabled
+					disable = { "ipynb", "latex" },
+					additional_vim_regex_highlighting = { "latex", "markdown" },
+					-- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
+					--disable = function(lang, buf)
+					--local max_filesize = 100 * 1024 -- 100 KB
+					--local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+					--if ok and stats and stats.size > max_filesize then
+					--return true
+					--end
+					--end,
 
-				-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-				-- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-				-- Using this option may slow down your editor, and you may see some duplicate highlights.
-				-- Instead of true it can also be a list of languages
-				--additional_vim_regex_highlighting = false,
+					-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+					-- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+					-- Using this option may slow down your editor, and you may see some duplicate highlights.
+					-- Instead of true it can also be a list of languages
+					--additional_vim_regex_highlighting = false,
+				},
+			}
+		end
+	},
+	{
+		"nvim-treesitter/nvim-treesitter-textobjects",
+		branch = "main",
+		init = function()
+			-- Disable entire built-in ftplugin mappings to avoid conflicts.
+			-- See https://github.com/neovim/neovim/tree/master/runtime/ftplugin for built-in ftplugins.
+			vim.g.no_plugin_maps = true
+
+			-- Or, disable per filetype (add as you like)
+			-- vim.g.no_python_maps = true
+			-- vim.g.no_ruby_maps = true
+			-- vim.g.no_rust_maps = true
+			-- vim.g.no_go_maps = true
+		end,
+		options = {
+			textobjects = {
+				move = {
+					enable = true,
+					set_jumps = true,
+					goto_next_start = {
+						["]]"] = "@cell.inner", -- Custom jump to next cell
+					},
+					goto_previous_start = {
+						["[["] = "@cell.inner",
+					},
+				},
 			},
 		}
-
-		-- Queries
-		--vim.treesitter.set_query("python", "folds", [[
-		--(
-		--(comment) @test
-		--(#match? @test "(^# %%)")
-		--) @fold
-		--]])
-
-	end
+	}
 }
